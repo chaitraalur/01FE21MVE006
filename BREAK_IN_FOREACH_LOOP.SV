fdd87c6803aff81fa6ee963b1ced74cbc3b577da
chaitraalur <102359148+chaitraalur@users.noreply.github.com>
2022-09-13 12:22:24 +0530

    Create BREAK_IN_FOREACH_LOOP.SV

---
+++ BREAK_IN_FOREACH_LOOP.SV
@@ -0,0 +1,17 @@
+// Code your testbench here
+// or browse Examples
+module foreach_loop_break;
+  int a[4];
+     initial begin
+    $display("-----------------------------------------------------------------");
+      foreach(a[i]) a[i] = i;
+    foreach(a[i]) begin
+      $display("\tValue of a[%0d]=%0d",i,a[i]);
+      if(i == 2) begin
+        $display("\tCalling break,");
+        break;
+      end 
+    end     
+         $display("-----------------------------------------------------------------");
+  end    
+endmodule
