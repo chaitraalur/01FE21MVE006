cbc169a16303beb123552cdccb42460d480f692d
chaitraalur <102359148+chaitraalur@users.noreply.github.com>
2022-09-13 13:35:04 +0530

    Create TASK.SV

---
+++ TASK.SV
@@ -0,0 +1,13 @@
+// Code your testbench here
+// or browse Examples
+module sv_task;
+  int x;
+   //task to add two integer numbers.
+  task sum(input int a,b,output int c);
+    c = a+b;  
+  endtask
+   initial  begin
+    sum(10,5,x);
+    $display("\tValue of x = %0d",x);
+  end
+endmodule
