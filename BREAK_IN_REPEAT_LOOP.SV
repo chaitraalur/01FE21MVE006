25573e48c3501c86ecd4a34863608bf01881acf4
chaitraalur <102359148+chaitraalur@users.noreply.github.com>
2022-09-13 12:24:59 +0530

    Create BREAK_IN_REPEAT_LOOP.SV

---
+++ BREAK_IN_REPEAT_LOOP.SV
@@ -0,0 +1,17 @@
+// Code your testbench here
+// or browse Examples
+module repeat_loop_break;
+  int i;
+     initial begin
+    $display("-----------------------------------------------------------------");
+repeat(5) begin
+  $display("\tValue of i=%0d",i);
+  if(i == 2) begin
+    $display("\tCalling break,");
+    break;
+  end
+      i++;
+    end     
+         $display("-----------------------------------------------------------------");
+  end    
+endmodule
